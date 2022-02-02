fb340a18ab7c5ca7f58092660e19183c106ce3b1
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2022-02-02 10:54:16 -0300

    Exercicios Bloco 25 dia 2 - JWT

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
+/* Aqui, importamos nossa função que valida se o usuário está ou não autenticado */
+const validateJWT = require('./auth/validateJWT');
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -11,10 +14,18 @@ app.use(bodyParser.json());
 
 const apiRoutes = express.Router();
 
-apiRoutes.get('/api/posts', routes.getPosts);
+// usamos esse middleware para adicionar autenticação na nossa rota de listagem de posts.
+apiRoutes.get('/api/posts', validateJWT, routes.getPosts);
+
+// apiRoutes.get('/api/posts', routes.getPosts);
+
 apiRoutes.post('/api/users', routes.createUsers);
 apiRoutes.get('/api/users', routes.getUsers);
 apiRoutes.post('/api/login', routes.login);
+
+/* Note que não queremos autenticar o login e nem criação de usuários, 
+  pois precisamos deles para o processo de autenticação! Se houvesse outras rotas 
+  protegidas na nossa aplicação, usaríamos o middleware nelas também! */
 
 app.use(apiRoutes);
 
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,29 @@
 const { Post } = require('../models');
 
-module.exports = async (_req, res) => {
-  const posts = await Post.findAll({ attributes: { exclude: 'id' } });
-  res.status(200).json({ mockPosts: posts });
-};
+// module.exports = async (_req, res) => {
+//   const posts = await Post.findAll({ attributes: { exclude: 'id' } });
+//   res.status(200).json({ mockPosts: posts });
+// };
+
+/* Mas como faríamos para recuperar apenas os posts do usuário logado?
+  Lembra-se de que o middleware de autenticação recupera o usuário do banco de dados 
+  e o coloca no req ? Esse objeto é o mesmo que é passado para todos os middlewares 
+  e para a callback da rota. Como o middleware de autenticação é executado antes das 
+  funções dos controllers, req conterá o usuário logado quando o controller em 
+  /controllers/posts for executado, e poderíamos utilizá-lo para fazer uma consulta 
+  ao banco de dados que trouxesse somente seus posts. 
+  Para confirmar isso, basta colocar um console.log dentro do controller: */
+
+  module.exports = async (req, res) => {
+    console.log(req.user.dataValues);
+    const posts = await Post.findAll({ attributes: { exclude: 'id' } });
+    res.status(200).json({ mockPosts: posts });
+  };
+
+// -------------------------------------
+// saída no terminal onde executou a API:
+// {
+//   id: 3,
+//   username: 'italssodj',
+//   password: 'senha123'
+// }
